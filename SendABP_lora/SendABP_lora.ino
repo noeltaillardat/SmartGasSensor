593653f49fa78fcc8d272292d1a0996959144bb5
fronarn <44197804+fronarn@users.noreply.github.com>
2019-11-19 12:54:32 +0100

    Test file to connect and test with ABP

---
+++ SendABP_lora/SendABP_lora.ino
@@ -0,0 +1,44 @@
+#include <TheThingsNetwork.h>
+#include <SoftwareSerial.h>
+
+// Set your DevAddr, NwkSKey, AppSKey and the frequency plan
+const char *devAddr = "26011ED8";
+const char *nwkSKey = "B4A356C5FBC0A4B24B4EF83D59A0CA06";
+const char *appSKey = "BEB8D4D7C052339D4510FD0FCD4D6E10";
+
+SoftwareSerial loraSerial(10, 11);
+#define debugSerial Serial
+
+#define freqPlan TTN_FP_EU868
+
+TheThingsNetwork ttn(loraSerial, debugSerial, freqPlan);
+
+void setup()
+{
+  loraSerial.begin(57600);
+  debugSerial.begin(9600);
+
+  // Wait a maximum of 10s for Serial Monitor
+  while (!debugSerial && millis() < 10000)
+    ;
+
+  debugSerial.println("-- PERSONALIZE");
+  ttn.personalize(devAddr, nwkSKey, appSKey);
+
+  debugSerial.println("-- STATUS");
+  ttn.showStatus();
+}
+
+void loop()
+{
+  debugSerial.println("-- LOOP");
+
+  // Prepare payload of 1 byte to indicate LED status
+  byte payload[1];
+  payload[0] = (digitalRead(LED_BUILTIN) == HIGH) ? 1 : 0;
+
+  // Send it off
+  ttn.sendBytes(payload, sizeof(payload));
+
+  delay(10000);
+}
